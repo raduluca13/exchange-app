eb7c7287b775cf157ee0820153d3bf9f6b997061
Radu Luca <radu.luca13@yahoo.com>
2021-06-05 14:14:49 +0300

    remove comments

--- src/screens/exchange/components/currency-input-container/CurrencyInputContainer.tsx
+++ src/screens/exchange/components/currency-input-container/CurrencyInputContainer.tsx
@@ -33,22 +33,17 @@ const CurrencyInputContainer = (props: CurrencyInputContainerProps) => {
     const onChangeAmount = (event: any) => {
         const stringValue = event.target.value.trim()
         const numberValue = +stringValue
-
         const isNumber = !isNaN(numberValue)
 
         const indexOfDelimiter = stringValue.indexOf('.') || stringValue.indexOf(',')
-        console.log({ indexOfDelimiter })
         const hasDelimiter = indexOfDelimiter !== -1
-        console.log({ hasDelimiter })
         const hasDelimiterOnlyOnLastPosition = ['.', ','].includes(stringValue[stringValue.length - 1])
-        console.log({ hasDelimiterOnlyOnLastPosition })
 
         if (!isNumber) {
             return
         }
 
         const maxTwoDigits = hasDelimiter && stringValue.length - indexOfDelimiter - 1 <= 2
-        console.log({ maxTwoDigits })
         if (!hasDelimiter || hasDelimiterOnlyOnLastPosition || maxTwoDigits) {
             setSelectedAmount(stringValue)
             return
